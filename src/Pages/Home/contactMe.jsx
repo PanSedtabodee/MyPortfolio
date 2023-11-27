3a3e8bff0c6e5ec5f9ba9a7cb78c04971038dc30
PanSedtabodee <sedtabodee.peterpan@mail.kmutt.ac.th>
2023-11-27 23:21:23 +0700

    :fire: Remove some html tag in contactMe.jsx

--- src/Pages/Home/contactMe.jsx
+++ src/Pages/Home/contactMe.jsx
@@ -2,8 +2,7 @@ export default function ContactMe() {
     return (
       <section id="contactMe" className="contact-me">
         <div>
-        <p>asdad</p>
-        <h1>asdasdd</h1>
+
         </div>
       </section>
     );
